d95417f457ef52ae626aa1f6a46fa48dc419361f
lauralee51 <lauralee51@gmail.com>
2017-04-23 17:31:51 -0400

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -30,63 +30,63 @@ $( document ).ready(function() {
       });
 
     // create function to listen for clicks on every "view details" button    
-        $(".view-details").on("click", function(event) {
-          // log event details
-          console.log(event);
-          // create variable to target event button
-          var targetElement = event.target;
-          // create variable to target grand-parent of "view-details" container div
-          var container = targetElement.parentElement.parentElement;
-          // within that container div, find all the elements that have the class "details"
-          $(container).find(".details").each(function(index, el) {
-          // toggle visibility of all elements within "details"
-          // if element is visible, fade out and change button text to View Details
-              if($(el).is(":visible")) {
-                $(el).fadeOut();
-                targetElement.innerText = "View Details";
-              } 
-          // if element is not visible, fade in and change button text to Hide Details
-              else {
-                $(el).fadeIn();
-                targetElement.innerText = "Hide Details";
-              }
-          });
+      $(".view-details").on("click", function(event) {
+    // log event details
+        console.log(event);
+    // create variable to target event button
+        var targetElement = event.target;
+    // create variable to target grand-parent of "view-details" container div
+        var container = targetElement.parentElement.parentElement;
+    // within that container div, find all the elements that have the class "details"
+        $(container).find(".details").each(function(index, el) {
+    // toggle visibility of all elements within "details"
+    // if element is visible, fade out and change button text to View Details
+          if($(el).is(":visible")) {
+          $(el).fadeOut();
+          targetElement.innerText = "View Details";
+          } 
+    // if element is not visible, fade in and change button text to Hide Details
+            else {
+          $(el).fadeIn();
+          targetElement.innerText = "Hide Details";
+          }
         });
+      });
 
-          // set up an event listener on the buttons with the `vote` class.
-         $(".vote").on("click", function(event) {
-             // set this up for switch statement
-              var self = this;
-              var $self = $(self);
-              var dataFunction = $self.attr('data-vote');
+    // set up an event listener on the buttons with the `vote` class.
+      $(".vote").on("click", function(event) {
+    // set this up for switch statement
+        var self = this;
+        var $self = $(self);
+        var dataFunction = $self.attr('data-vote');
            
-           switch (dataFunction) {
-               // if user click on great
-             case 'great':
-               // increment great and total in voteCounts
-             var newGreat = voteCounts.great += 1;
-             var newTotal = voteCounts.total += 1;
-             console.log(voteCounts);
-               // affect bar in HTML
-             var currentWidth = $(".great-progress").width();
-             var newWidth = currentWidth + (currentWidth * (newGreat / newTotal)) / 100;
-             $(".great-progress").width(newWidth);
-             break;
-               // if user clicks on greatest
-             case 'greatest':
+        switch (dataFunction) {
+    // if user clicks on great
+          case 'great':
+    // increment great and total in voteCounts
+            var newGreat = voteCounts.great += 1;
+            var newTotal = voteCounts.total += 1;
+            console.log(voteCounts);
+    // affect bar in HTML
+            var currentWidth = $(".great-progress").width();
+            var newWidth = currentWidth + (currentWidth * (newGreat / newTotal)) / 100;
+            $(".great-progress").width(newWidth);
+            break;
+    // if user clicks on greatest
+          case 'greatest':
                // increment greatest and total in voteCounts
-             var newGreatest = voteCounts.greatest += 1;
-             var newTotals = voteCounts.total += 1;
-             console.log(voteCounts);
-               // affect bar in HTML
-             var currentWidths = $(".greatest-progress").width();
-             var newWidths = currentWidths + (currentWidths * (newGreatest / newTotals)) / 100;
-             $(".greatest-progress").width(newWidths);
-             break;
-               // else do nothing
-             default: 
-               // do nothing
-             break;
-           }
-         });
+            var newGreatest = voteCounts.greatest += 1;
+            var newTotals = voteCounts.total += 1;
+            console.log(voteCounts);
+    // affect bar in HTML
+            var currentWidths = $(".greatest-progress").width();
+            var newWidths = currentWidths + (currentWidths * (newGreatest / newTotals)) / 100;
+            $(".greatest-progress").width(newWidths);
+            break;
+    // else do nothing
+            default: 
+              // do nothing
+            break;
+         }
+     });
 });
